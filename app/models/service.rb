2a2dcf5c22244cba9dc10818d56bcf9a2bce18fa
Eduardo Sánchez Yépiz <gallo.sanchez23@gmail.com>
2018-05-29 19:17:35 -0500

    Creates a rollback preparation on project

--- app/models/service.rb
+++ app/models/service.rb
@@ -1,7 +1,4 @@
 class Service < ApplicationRecord
-	has_attached_file :image_2, styles: { large: "600x600>", medium: "300x300>", thumb: "100x100>" }
-	validates_attachment_content_type :image_2, content_type: /\Aimage\/.*\z/
-
 	has_many :comments
 	has_many :services_promotions
 	has_many :services_classifications
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180529224527) do
+ActiveRecord::Schema.define(version: 20171109205248) do
 
   create_table "classifications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "title"
@@ -45,10 +45,6 @@ ActiveRecord::Schema.define(version: 20180529224527) do
     t.datetime "updated_at", null: false
     t.string "image"
     t.text "description"
-    t.string "image_2_file_name"
-    t.string "image_2_content_type"
-    t.integer "image_2_file_size"
-    t.datetime "image_2_updated_at"
   end
 
   create_table "services_classifications", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
